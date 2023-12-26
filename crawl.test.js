c56d5e81372fddd4d34f4d83a1b21d929c68053d
egcutler <ericgcutler@gmail.com>
2023-12-26 13:22:48 -0600

    normalizeURL and tests

--- crawl.test.js
+++ crawl.test.js
@@ -7,3 +7,24 @@ test('normalizeURL strip protocol', () => {
       const expected = 'blog.boot.dev/path'
       expect(actual).toEqual(expected)
 })
+
+test('normalizeURL strip trailing slash (/)', () => {
+      const input = 'https://blog.boot.dev/path/'
+      const actual = normalizeURL(input)
+      const expected = 'blog.boot.dev/path'
+      expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip capitals', () => {
+      const input = 'https://BLOG.boot.dev/path/'
+      const actual = normalizeURL(input)
+      const expected = 'blog.boot.dev/path'
+      expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip https to http', () => {
+      const input = 'http://blog.boot.dev/path/'
+      const actual = normalizeURL(input)
+      const expected = 'blog.boot.dev/path'
+      expect(actual).toEqual(expected)
+})
